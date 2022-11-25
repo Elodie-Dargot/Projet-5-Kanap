cd3cb1cfcd9a98c0ea15e4d0a3c121fe472422e1
Elodie Dargot <elodie.dargot@gmail.com>
2022-11-25 22:40:47 +0100

    modification page panier

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,5 +1,5 @@
 let str = window.location.href;
 let url = new URL(str);
 let id = url.searchParams.get("order");
-let test = document.getElementById('orderId');
-test.innerText = id;
+let orderId = document.getElementById('orderId');
+orderId.innerText = id;
